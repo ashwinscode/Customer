92d963a5020de385d9004f75a1533a000a8a4692
Ashwin <Ashwin@DESKTOP-53C91J8>
2024-01-12 10:59:22 -0500

    Add project files.

---
+++ Customer.API/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.API.Controllers
+{
+    [Route("customer")]
+    public class CustomerController : Controller
+    {
+        public CustomerController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get(string id)
+        {
+            return "Name - Get";
+        }
+
+        [HttpPost]
+        public string Post(string id)
+        {
+            return "Name - Post";
+        }
+
+        [HttpPut]
+        public string Update(string id)
+        {
+            return "Name - Update";
+        }
+
+        [HttpDelete]
+        public string Delete(string id)
+        {
+            return "Name - Delete";
+        }
+    }
+}
---
+++ Customer.API/Program.cs
@@ -0,0 +1,46 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+//app.UseStaticFiles();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+//var summaries = new[]
+//{
+//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+//};
+
+//app.MapGet("/weatherforecast", () =>
+//{
+//    var forecast = Enumerable.Range(1, 5).Select(index =>
+//        new WeatherForecast
+//        (
+//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//            Random.Shared.Next(-20, 55),
+//            summaries[Random.Shared.Next(summaries.Length)]
+//        ))
+//        .ToArray();
+//    return forecast;
+//});
+
+app.Run();
+
+//internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+//{
+//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//}
